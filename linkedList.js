b5a8b36d3b75f4894c18d13c3b77925c05116d7a
Justin Lee <leejwg.3@gmail.com>
2017-09-14 14:04:09 -1000

    starting header for nodes

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,99 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
 
+function linkedListGenerator(){
+  this.head = null;
+  var length = 0;
+  var head = null;
+
+  // function state(){
+  //   current = 0;
+  //   next = current++;
+  // }
+
+  function getHead(){
+    // body...
+  }
+
+
+
+  function getTail(){
+    // body...
+  }
+
+
+
+
+  function remove() {
+    // body...
+  }
+
+
+
+  function add(value){
+    // body...
+  }
+
+
+
+
+  function getNumber(number){
+    // body...
+  }
+
+
+
+
+  function removeNumber(number){
+    // body...
+  }
+
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    removeNumber: removeNumber,
+    remove: remove,
+    add: add,
+    getNumber: getNumber,
+
+
+
+  };
 }
+
+
+
+
+
+
+
+  // function getHead(){
+  //   var linkedList = {
+  //     value: 'Ready Player One',
+  //     next: {
+  //       value: '1982',
+  //       next: {
+  //         value: 'Neuromancer',
+  //         next: {
+  //           value: 'Snow Crash',
+  //           next: null
+  //         }
+  //       }
+  //     }
+  //   };
+  //   return head[value];
+  // }
+
+  // function getTail(){
+  //   value:  'Snow Crash';
+  //   next: null;
+  // }
+
+
+    
+  //   return {
+  //     getHead: getHead,
+  //     getTail: getTail
+  //   };
